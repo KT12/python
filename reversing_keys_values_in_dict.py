cd9255e634df702f2090e290e175e6eaa1c59530
KT12 <nyy2008@gmail.com>
2016-08-11 23:18:56 -0400

    Update reversing_keys_values_in_dict.py

--- reversing_keys_values_in_dict.py
+++ reversing_keys_values_in_dict.py
@@ -3,3 +3,6 @@
 random_dict = {'z':0, 'y':1, 'x':2, 'w':3}
 
 {value:key for key,value in random_dict.items()}
+
+# This only works if the values of the dict are immutable.
+# Procedure fails for lists in the dict
