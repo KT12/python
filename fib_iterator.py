e59ce99fa5753d5758cfd2e057bcc7dc22271368
KT12 <nyy2008@gmail.com>
2016-08-12 14:03:32 -0400

    Update fib_iterator.py

--- fib_iterator.py
+++ fib_iterator.py
@@ -1,1 +1,15 @@
 class Fib:
+  def __init__(self, maximum):
+    self.maximum = maximum
+  
+  def __iter__(self):
+    self.a = 0
+    self.b = 1
+    return self
+    
+  def __next__(self):
+    fib = self.a
+    if fib > self.max:
+      raise StopIteration
+    self.a, self.b = self.b, self.a + self.b
+    return fib
