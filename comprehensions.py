4ce253b2c73143707c698c3983915d92f0b1d889
KT12 <nyy2008@gmail.com>
2016-08-16 15:03:08 -0400

    Add files via upload

--- comprehensions.py
+++ comprehensions.py
@@ -30,3 +30,40 @@ map(lambda e: e**2, a_list)
 # Combine map and filter, use list() to assign to list
 list(map(lambda e: e**2, filter(lambda e: type(e)==int, a_list)))
 
+'''
+Nested comprehensions
+Can use a nested comprehension to create Identity matrix n*n
+'''
+n = 3
+
+I = [[1 if item_idx == row_idx else 0 for item_idx in range(n)] for row_idx in range(n)]
+
+# Upper triangle matrix
+
+U = [[1 if item_idx >= row_idx else 0 for item_idx in range(n)] for row_idx in range(n)]
+
+# Lower triangle matrix
+
+L = [[1 if item_idx <= row_idx else 0 for item_idx in range(n)] for row_idx in range(n)]
+
+# Hollow matrix
+
+H = [[1 if item_idx != row_idx else 0 for item_idx in range(n)] for row_idx in range(n)]
+
+
+''' Set Comprehensions '''
+
+names = ['Omar', 'JOHN', 'alice', 'Taku', 'Jimi', 'bob', 'ALICE', 'J', 'Bob', 'T']
+
+# Return set of all names with only the first letter capitalized for all names longer than 1 letter
+
+tags = {name[0].upper() + name[1:].lower() for name in names if len(name) > 1}
+
+
+''' Dict Comprehensions '''
+
+# Return dict which combines character counts that are not case sensitive
+
+case_freq = {'a':10, 'b': 34, 'z': 51, 'A': 7, 'C': 42, 'Z':3}
+
+total_freq = {k.lower(): case_freq.get(k.lower(),0)+case_freq.get(k.upper(),0) for k in case_freq.keys()}
